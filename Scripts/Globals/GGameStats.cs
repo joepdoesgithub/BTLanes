723dc87c342108b18f213b136830bb2fd21388a6
joepdoesgithub <jfennema@gmail.com>
2020-03-20 16:35:35 +0100

    Added GGameStats

---
+++ Scripts/Globals/GGameStats.cs
@@ -0,0 +1,7 @@
+public static class GGameStats{
+	public static readonly int WalkHeat = 1;
+	public static readonly int RunHeat = 2;
+	public static readonly int JumpHeatPerLane = 2;
+
+	public static readonly int[] RangeModifier = {-1,0,2,4};
+}
